e7170db3f94962a0a8dc9842a7045105b8ea959b
Marius Sørensen <jobb@s0rensen.no>
2020-11-10 20:45:21 +0100

    Added GameStore and GameStoreTests

---
+++ AvalonApiNoDB.Core/Domain/Games/GameStore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvalonApiNoDB.Core.Domain.Games
+{
+    public sealed class GameStore
+    {
+        private static readonly GameStore instance = new GameStore();
+        public Dictionary<Guid, Game> Games;
+        static GameStore()
+        {
+        }
+
+        private GameStore()
+        {
+            Games = new Dictionary<Guid, Game>();
+        }
+
+        public static GameStore Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public static void AddGame(Game g)
+        {
+            instance.Games[g.Id] = g;
+        }
+
+        public static Game GetGame(Guid id)
+        {
+            if(instance.Games.TryGetValue(id, out Game g))
+                return g;
+            return null;
+        }
+
+        public static void DeleteGame(Guid id)
+        {
+            instance.Games.Remove(id);
+        }
+
+        public static Game GetGameByJoinCode(int joinCode)
+        {
+            Game g = instance.Games.Where(keyValuePair => keyValuePair.Value.JoinCode == joinCode).FirstOrDefault().Value;
+            if (g == default(Game))
+                throw new KeyNotFoundException($"Game with Join Code {joinCode} does not exist");
+            return g;
+        }
+
+        public static void Reset()
+        {
+            instance.Games = new Dictionary<Guid, Game>();
+        }
+    }
+}
---
+++ AvalonApiNoDB.Tests/Core/Games/GameStoreTests.cs
@@ -0,0 +1,73 @@
+﻿using AvalonApiNoDB.Core.Domain.Games;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AvalonApiNoDB.Tests.Core.Games
+{
+    [TestClass]
+    public class GameStoreTests
+    {
+        private Game SetupGameStoreWithSingleGame()
+        {
+            Game g = new Game();
+            GameStore.Instance.Games[g.Id] = g;
+
+            return g;
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            GameStore.Reset();
+        }
+
+        [TestMethod]
+        public void AddGame()
+        {
+            Game g = new Game();
+            GameStore.AddGame(g);
+
+            Assert.AreEqual(g, GameStore.Instance.Games[g.Id]);
+        }
+
+        [TestMethod]
+        public void GetGame()
+        {
+            Game g = SetupGameStoreWithSingleGame();
+
+            Assert.AreEqual(g, GameStore.GetGame(g.Id));
+        }
+
+        [TestMethod]
+        public void DeleteGame()
+        {
+            Game g = SetupGameStoreWithSingleGame();
+
+            GameStore.DeleteGame(g.Id);
+
+            Assert.AreEqual(0, GameStore.Instance.Games.Keys.Count);
+        }
+
+        [TestMethod]
+        public void DeleteGameWithMultipleOtherGames()
+        {
+            Game g = SetupGameStoreWithSingleGame();
+            Game g2 = SetupGameStoreWithSingleGame();
+            Game g3 = SetupGameStoreWithSingleGame();
+
+            GameStore.DeleteGame(g.Id);
+
+            Assert.IsFalse(GameStore.Instance.Games.ContainsValue(g));
+            Assert.IsTrue(GameStore.Instance.Games.ContainsValue(g2));
+            Assert.IsTrue(GameStore.Instance.Games.ContainsValue(g3));
+        }
+
+        [TestMethod]
+        public void GetGameByJoinCode()
+        {
+            Game g = SetupGameStoreWithSingleGame();
+
+            Assert.AreEqual(g, GameStore.GetGameByJoinCode(g.JoinCode));
+        }
+    }
+}
