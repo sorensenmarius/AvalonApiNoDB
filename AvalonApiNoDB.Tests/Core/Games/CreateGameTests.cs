32da0dd10beb48d022f616e04c3c17fda4bad79e
Marius Sørensen <jobb@s0rensen.no>
2020-11-10 18:07:31 +0100

    Added tests

---
+++ AvalonApiNoDB.Tests/Core/Games/CreateGameTests.cs
@@ -0,0 +1,26 @@
+﻿using AvalonApiNoDB.Core.Domain.Games;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AvalonApiNoDB.Tests.Core.Games
+{
+    [TestClass]
+    public class CreateGameTests
+    {
+        [TestMethod]
+        public void IdIsNotNull()
+        {
+            Game g = new Game();
+
+            Assert.AreNotEqual(g.Id, new Guid());
+        }
+        
+        [TestMethod]
+        public void GameStatusIsWaitingForPlayers()
+        {
+            Game g = new Game();
+
+            Assert.AreEqual(g.Status, GameStatus.WaitingForPlayers);
+        }
+    }
+}
