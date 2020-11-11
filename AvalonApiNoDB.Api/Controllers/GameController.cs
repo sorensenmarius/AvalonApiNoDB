168ed475c4a53788b3c97f03b6d4d4ca79ec98e7
Marius Sørensen <jobb@s0rensen.no>
2020-11-11 21:42:28 +0100

    Added GameController

---
+++ AvalonApiNoDB.Api/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using AvalonApiNoDB.Core.Domain.Games;
+using AvalonApiNoDB.Core.Domain.Players;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvalonApiNoDB.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        // GET api/<GameController>/5
+        [HttpGet("{id}")]
+        public Game Get(Guid id)
+        {
+            return GameStore.GetGame(id);
+        }
+
+        [HttpPost]
+        public Game JoinGame(int joinCode, string playerName)
+        {
+            Game g = GameStore.GetGameByJoinCode(joinCode);
+
+            Player p = new Player(playerName);
+
+            g.Players.Add(p);
+
+            return g;
+        }
+    }
+}
