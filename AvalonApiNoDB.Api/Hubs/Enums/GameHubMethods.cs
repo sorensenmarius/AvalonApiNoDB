95dae909af85e1d312b00ea349998ad4770c6bab
Marius Sørensen <jobb@s0rensen.no>
2021-02-05 18:06:56 +0100

    Started SignalR

---
+++ AvalonApiNoDB.Api/Hubs/Enums/GameHubMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvalonApiNoDB.Api.Hubs.Enums
+{
+    struct GameHubMethods
+    {
+        public const string PlayerJoined = "playerJoined";
+    }
+}
--- AvalonApiNoDB.Api/Hubs/GameHub.cs
+++ AvalonApiNoDB.Api/Hubs/GameHub.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AvalonApiNoDB.Api.Hubs.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,5 +10,20 @@ namespace AvalonApiNoDB.Api.Hubs
     public class GameHub : Hub
     {
 
+        public async Task Test()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "abcabc");
+        }
+        public async Task HostGame(string gameId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+        }
+
+        public async Task JoinGame(string gameId, string playerName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+
+            await Clients.Group(gameId).SendAsync(GameHubMethods.PlayerJoined, playerName);
+        }
     }
 }
