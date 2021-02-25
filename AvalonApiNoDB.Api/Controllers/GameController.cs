595a0a525ecb8b07ef5bef07d346836ff6254142
Marius Sørensen <jobb@s0rensen.no>
2021-02-25 17:57:26 +0100

    PLEASE MANN

--- AvalonApiNoDB.Api/Controllers/GameController.cs
+++ AvalonApiNoDB.Api/Controllers/GameController.cs
@@ -62,7 +62,7 @@ namespace AvalonApiNoDB.Api.Controllers
         [HttpGet("TESTING")]
         public string Testing()
         {
-            return "FUCK YEAhhhhhhhh";
+            return "FUCK YEAhhhhhhhh???";
         }
     }
 }
